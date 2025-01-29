fe2f07f2502550e47e16829ef8dde5b20d83fc17
MbNextT1me <thecolouroutofspase@gmail.com>
2025-01-29 17:23:24 +0800

    init project, add main configs

---
+++ src/test/java/ru/gormikle/interviewapp/InterviewAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gormikle.interviewapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
