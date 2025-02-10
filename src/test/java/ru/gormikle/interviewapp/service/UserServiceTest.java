fa743bf29fb015a5efd5d828cb421ae04d2fd84d
MbNextT1me <thecolouroutofspase@gmail.com>
2025-02-11 01:06:17 +0800

    add read operations + tests

--- src/test/java/ru/gormikle/interviewapp/service/UserServiceTest.java
+++ src/test/java/ru/gormikle/interviewapp/service/UserServiceTest.java
@@ -1,6 +1,5 @@
 package ru.gormikle.interviewapp.service;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
